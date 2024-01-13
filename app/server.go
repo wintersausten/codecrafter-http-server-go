5a786b00a3101149e8a96d04aba2ecd973ac8681
wintersausten <wintersausten@gmail.com>
2024-01-13 15:48:58 -0800

    read data from connection & write 200 response

--- app/server.go
+++ app/server.go
@@ -13,9 +13,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+  conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+  defer conn.Close()
+
+  connBuffer := make([]byte, 4096)
+  _, err = conn.Read(connBuffer)
+	if err != nil {
+		fmt.Println("Error reading data from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+  response := "HTTP/1.1 200 OK\r\n\r\n"
+  _, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing data to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
