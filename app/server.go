d0db2ec33a78df273683d852bfcdcef85bf9952b
wintersausten <wintersausten@gmail.com>
2024-01-13 17:40:53 -0800

    handle connection concurrently

--- app/server.go
+++ app/server.go
@@ -23,11 +23,17 @@ func main() {
 		os.Exit(1)
 	}
 
-  conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+  for {
+    conn, err := l.Accept()
+    if err != nil {
+      fmt.Println("Error accepting connection: ", err.Error())
+      continue
+    }
+    go handleConnection(conn)
+  }
+}
+
+func handleConnection(conn net.Conn) {
   defer conn.Close()
 
   connReader := bufio.NewReader(conn)
